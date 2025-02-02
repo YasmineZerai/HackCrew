b2836e09e28c9febd82e5af40295c4117bc008af
YasmineZerai <zeraiyasmine7@gmail.com>
2025-02-02 14:03:08 +0100

    installed mongoose and mongoDB and connected to the database

--- HackCrew-Back/src/index.ts
+++ HackCrew-Back/src/index.ts
@@ -1,12 +1,25 @@
-import express, { Request, Response, response } from "express";
+import express, { Request, Response } from "express";
 import { createServer } from "node:http";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 const app = express();
 const server = createServer(app);
+const mongodbString = process.env.MONGO_DB_URL;
 app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`);
-});
+if (mongodbString !== undefined) {
+  mongoose
+    .connect(mongodbString)
+    .then((value) => {
+      console.log("connected to the database succesfully");
+      const port = process.env.PORT;
+      server.listen(port, () => {
+        console.log(`listenning on localhost:${port} `);
+      });
+    })
+    .catch((error) => {
+      console.log("an error occured while connecting to the database");
+      process.exit(1);
+    });
+}
