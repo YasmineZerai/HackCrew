9c48f9c1f6e1aea15af6d67c7997b2ea9e8180ba
YasmineZerai <zeraiyasmine7@gmail.com>
2025-02-02 17:03:10 +0100

    created a validation middleware

---
+++ HackCrew-Back/src/middlewares/validate.ts
@@ -0,0 +1,26 @@
+import { NextFunction, query, Request, Response } from "express";
+import { z } from "zod";
+
+export const validation = (schema: z.AnyZodObject) => {
+  const validationMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const validationResult = schema.safeParse({
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    });
+    if (!validationResult.success) {
+      const payload: Record<string, string> = {};
+      for (const error of validationResult.error.errors) {
+        payload[error.path[error.path.length - 1]] = error.message;
+      }
+      next({
+        status: 400,
+        message: "this error happened while validating your request",
+      });
+    }
+  };
+};
