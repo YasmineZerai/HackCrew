d3f1612bf1be4f47fd9208941a608ce3e8b49f7f
YasmineZerai <zeraiyasmine7@gmail.com>
2025-02-02 17:41:33 +0100

    created the create user service

---
+++ HackCrew-Back/src/services/users.ts
@@ -0,0 +1,31 @@
+import { createUser, getUserByEmail } from "../database/users";
+import bcrypt from "bcrypt";
+
+type CreateUserArgs = {
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+};
+export async function createUserService(args: CreateUserArgs) {
+  const existingUser = await getUserByEmail(args.email);
+  if (existingUser)
+    return {
+      status: 400,
+      success: false,
+      message: "user already exists",
+    };
+  const hashedPassword = await bcrypt.hash(args.password, 10);
+  const newUser = await createUser({
+    firstName: args.firstName,
+    lastName: args.lastName,
+    email: args.email,
+    password: hashedPassword,
+  });
+  return {
+    success: true,
+    status: 201,
+    message: "user created successfully",
+    payload: { newUser },
+  };
+}
