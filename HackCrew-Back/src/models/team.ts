a337810fda4e229a0aecd0cc05f54f418bb63617
YasmineZerai <zeraiyasmine7@gmail.com>
2025-02-08 14:01:44 +0100

    changed the team model schema

--- HackCrew-Back/src/models/team.ts
+++ HackCrew-Back/src/models/team.ts
@@ -4,12 +4,10 @@ import { User } from "./user";
 export interface Team {
   _id: mongoose.Types.ObjectId;
   teamName: String;
-  members: User[] | String[];
 }
 
 const TeamSchema = new mongoose.Schema<Team>({
   teamName: { type: String, required: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
 });
 
 export const TeamModel = mongoose.model("teams", TeamSchema);
