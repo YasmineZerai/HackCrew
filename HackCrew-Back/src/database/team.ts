5e366244dbd35dd6cbd537b7e96e90af7a71dc96
YasmineZerai <zeraiyasmine7@gmail.com>
2025-02-08 14:18:56 +0100

    fixed the database file for teams

--- HackCrew-Back/src/database/team.ts
+++ HackCrew-Back/src/database/team.ts
@@ -4,37 +4,29 @@ import { CodeModel } from "../models/code";
 
 interface createTeamArgs {
   teamName: String;
-  // code: String;
-  members: String[];
 }
+
 export async function createTeam(args: createTeamArgs) {
   return await TeamModel.create({
-    members: args.members,
     teamName: args.teamName,
   });
 }
+
 export async function getTeamById(teamId: string) {
   return await TeamModel.findById(new mongoose.Types.ObjectId(teamId));
 }
+
 export async function getTeamCode(teamId: String) {
   return await CodeModel.findOne({ team: teamId });
 }
+
 export async function createTeamCode(teamId: string, teamCode: string) {
   return await CodeModel.create({
     team: teamId,
     code: teamCode,
   });
 }
-export async function joinTeam(userId: string, teamId: string) {
-  return await TeamModel.findByIdAndUpdate(
-    teamId,
-    { $addToSet: { members: new mongoose.Types.ObjectId(userId) } }, // Convert to ObjectId
-    { new: true }
-  );
-}
+
 export async function getTeamByCode(code: string) {
   return await CodeModel.findOne({ code });
-}
-export async function getTeamsByUserId(userId: string) {
-  return await TeamModel.find({ members: new mongoose.Types.ObjectId(userId) });
 }
