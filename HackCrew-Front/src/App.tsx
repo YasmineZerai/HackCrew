f61350cc87c12ba280b66c0059a6fdd652022b25
YasmineZerai <zeraiyasmine7@gmail.com>
2025-02-11 22:33:35 +0100

    added the pages in the app

--- HackCrew-Front/src/App.tsx
+++ HackCrew-Front/src/App.tsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router";
 import RegisterPage from "./pages/register";
+import { Toaster } from "@/components/ui/sonner";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
-    <Routes>
-      <Route path="Register" element={<RegisterPage />} />
-    </Routes>
+    <>
+      <Toaster className="bg-coll2-teal-200 text-white" />
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
   );
 }
 
