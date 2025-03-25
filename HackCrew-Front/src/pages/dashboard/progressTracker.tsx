83d279cc525be6b6f7b49c91e1696ee1bedca74e
YasmineZerai <zeraiyasmine7@gmail.com>
2025-03-25 11:48:42 +0100

    created the new pages

--- HackCrew-Front/src/pages/dashboard/progressTracker.tsx
+++ HackCrew-Front/src/pages/dashboard/progressTracker.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function ProgressTracker() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4  bg-coll6-purple-100 ">
       <div className="min-h-[100vh] flex-1 rounded-xl md:min-h-min bg-muted/50">
-        home
+        ProgressTracker
       </div>
     </div>
   );
