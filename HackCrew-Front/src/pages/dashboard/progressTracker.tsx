824d9eff8ae84f02856409a05dadaa406cb59114
YasmineZerai <zeraiyasmine7@gmail.com>
2025-04-29 21:29:56 +0100

    added a team ressources component

--- HackCrew-Front/src/pages/dashboard/progressTracker.tsx
+++ HackCrew-Front/src/pages/dashboard/progressTracker.tsx
@@ -1,8 +1,12 @@
+import TeamRessources from "@/components/progress-tracker-components/teamRessources";
+import TeamUpdates from "../../components/progress-tracker-components/teamUpdates";
+
 export default function ProgressTracker() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4  bg-coll6-purple-100 ">
-      <div className="min-h-[100vh] flex-1 rounded-xl md:min-h-min bg-muted/50">
-        ProgressTracker
+      <div className="min-h-[80vh] flex gap-2 rounded-xl  bg-muted/50 p-2">
+        <TeamUpdates />
+        <TeamRessources />
       </div>
     </div>
   );
